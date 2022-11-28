b1114f92057df5e5f2d23d6ee7aabdae3dd43cbc
AImenes <aimenes@icloud.com>
2022-11-28 20:48:11 +0100

    problem 32

--- python/problem123.py
+++ python/problem123.py
@@ -1,24 +1,47 @@
 import math
-import sympy
+import time
 
 def prob123():
-	for n in range(3,100):
-		p_n = sympy.prime(n)
-		if not n % 2 == 0:
-			rest = (((p_n - 1)**n)+((p_n + 1)**n)) % (p_n**2)
-			print(n, p_n, rest)
+	n, r = 21000, 0
+	prime_list = primes(int(3e5))
+	
+	while r < 1e10:
+		p_n = prime_list[n]
+		r = 2 * p_n * n	
+		n += 1
+	return n	
 
-			if rest > 1e5:
-				print("Yo", n)	
+
+#sieve of Eratosthenes
+def primes(n):
+	if n > 2:
+		numbers = [True for i in range(n+1)]
+		numbers[0], numbers[1] = False, False
+		primes = [0]
+
+	elif n == 2:
+		numbers = [2]
+	else:
+		return -1
+
+	for i in range(n+1):
+		k = 2
+		if numbers[i]:
+			primes.append(i)
+
+			while (k * i) <= n:
+				numbers[k*i] = False
+				k += 1
+
+	return primes
+
 
 def main():
-	prob123()
+	start = time.time()
+	print(prob123())
+	end = time.time()
+	print(end-start,"s")
 
 if __name__ == "__main__":
 	main()
 
-
-# 32
-# 48
-# 64
-# 
--- python/problem32.py
+++ python/problem32.py
@@ -7,4 +7,50 @@ Find the sum of all products whose multiplicand/multiplier/product identity can 
 
 HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 '''
+def prob32():
+	count = 0
+	sum_of_pandigitals = 0
+	products = list()
+	for x in range(2,99):
+		for y in range(101,5000):
+			valid = True
+			utilized = [False for i in range(10)]
+			utilized[0] = True
+			x_str = [int(i) for i in str(x)]
 
+			for digit in x_str:
+					if utilized[digit] == False:
+						utilized[digit] = True
+					else:
+						valid = False
+
+			y_str = [int(i) for i in str(y)]
+			for digit in y_str:
+				if utilized[digit] == False:
+					utilized[digit] = True
+				else:
+					valid = False
+
+			product = x * y
+			product_str = [int(i) for i in str(product)]
+			
+			for digit in product_str:
+				if utilized[digit] == False:
+					utilized[digit] = True
+				else:
+					valid = False
+
+			if valid and False not in utilized:
+				if product not in products:
+					count += 1
+					products.append(product)
+					print(x,y,product)
+					sum_of_pandigitals += product
+
+	return count, sum_of_pandigitals
+
+def main():
+	print(prob32())
+
+if __name__ == "__main__":
+	main()
